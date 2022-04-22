a308073f1aeba6a5afb37c9c6bfec7692c1cd6eb
Greg Shabanov <30425203+yegorka78@users.noreply.github.com>
2022-04-22 14:02:03 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import './App.css'
+import CarsContainer from './cars/carsContainer'
+function App() {
+  return (
+    <div className="App">
+      <CarsContainer />
+    </div>
+  )
+}
+export default App
